c2baa88d27b7f6fe92b6920ff02a41fb2f746ff1
Eden Azulay <88677472+EdenAzu@users.noreply.github.com>
2024-03-31 00:27:45 +0300

    Add files via upload

--- server/models/codeblock.js
+++ server/models/codeblock.js
@@ -1,1 +1,12 @@
+//Defining a Mongoose schema and model for a CodeBlock document in my MongoDB database.
+const mongoose = require('mongoose');
 
+const codeBlockSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  code: { type: String, required: true },
+  solution: {type: String, required: true}
+});
+
+const CodeBlock = mongoose.model('CodeBlock', codeBlockSchema);
+
+module.exports = CodeBlock;
